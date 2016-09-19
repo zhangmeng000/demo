c28ca11593542856c55b61d687c5a50e0ca807eb
zhangmeng000 <1550685677@qq.com>
2016-09-19 09:49:18 +0800

    9-19

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # little-demo
 1606脱产小demo
+
+```js
+
+import axios from 'axios';
+function searchGit(add){
+  let address = `https://api.github.com/users/${add}`
+  return axios.get(address)
+  .then((res) => (
+    {  getData:res.data }
+  ))
+  .catch(function (error) {
+    alert(error);
+  });
+}
+export { searchGit }
+
+```
--- src/Home.js
+++ src/Home.js
@@ -10,7 +10,7 @@ class Home extends React.Component {
     return(
       <div className="jumbotron">
         <h1>欢迎光临</h1>
-        <p>here is Newming 的 Concer</p>
+        <p>here is 张盟 的 Concer</p>
         <p><a className="btn btn-primary btn-lg" role="button" onClick={this.handleClick.bind(this)}>我的工作</a></p>
       </div>
     )
